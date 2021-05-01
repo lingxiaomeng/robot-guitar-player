169ace608786e6b3cfd8aa7a964f12580ca15a0d
lingxiaomeng <574243996@qq.com>
2021-04-30 22:52:48 -0700

    get image v1.0

--- image/change_Image.py
+++ image/change_Image.py
@@ -10,6 +10,19 @@ K_left = [606.4096069335938, 0.0, 322.3016052246094, 0.0, 0.0, 606.3513793945312
           1.0, 0.0]
 K_right = [614.1268310546875, 0.0, 321.2118835449219, 0.0, 614.7515869140625, 234.63755798339844, 0.0, 0.0, 1.0]
 
+
+def get_H():
+    R = np.array(
+        [[sqrt(0.5), -sqrt(0.375), sqrt(0.125)],
+         [sqrt(0.5), sqrt(0.375), -sqrt(0.125)],
+         [0, 0.5, sqrt(0.75)]])
+    T = (np.array([[-0.287954], [0.178989], [0.0824499]]))
+    d = 0.4205 - 0.05
+    N = (np.array([[0], [0.5], [sqrt(0.75)]]))
+    H = R + (T / d).dot(N.transpose())
+    return H
+
+
 if __name__ == "__main__":
 
     K_left = np.array([[606.4096069335938, 0., 322.3016052246094],
@@ -19,31 +32,14 @@ if __name__ == "__main__":
     K_right = np.array([[614.1268310546875, 0., 321.2118835449219],
                         [0., 614.7515869140625, 234.63755798339844],
                         [0., 0., 1.]])
-
-    R = np.array(
-        [[sqrt(0.5), -sqrt(0.5), 0],
-         [sqrt(0.375), sqrt(0.375), -0.5],
-         [sqrt(0.125), sqrt(0.125), sqrt(0.75)]])
-
-    print(np.linalg.det(R))
-
-    T = (np.array([[-0.287954], [0.178989], [0.0824499]]))
-
-    d = 0.4205 - 0.05
-    # dinv = 1 / d
-
-    N = (np.array([[0], [0.5], [sqrt(0.75)]]))
-
-    H = R + (T / d).dot(N.transpose())
-    # print H
-    K_top_left = np.array([[606.4096069335938, 0., 1000],
-                           [0., 606.3513793945312, 600],
+    K_top_left = np.array([[606.4096069335938, 0., 800],
+                           [0., 606.3513793945312, 250],
                            [0., 0., 1.]])
 
-    K_top_right = np.array([[614.1268310546875, 0., 1000],
-                            [0., 614.7515869140625, 600],
+    K_top_right = np.array([[614.1268310546875, 0., 480],
+                            [0., 614.7515869140625, 250],
                             [0., 0., 1.]])
-
+    H = get_H()
     Homography_left = K_top_left.dot(H).dot(np.linalg.inv(K_left))
 
     Homography_right = K_top_right.dot(H).dot(np.linalg.inv(K_right))
@@ -59,9 +55,9 @@ if __name__ == "__main__":
         print(name)
         img = cv.imread(fname)
         if 'left' in name:
-            out = cv.warpPerspective(img, Homography_left, (1200, 1000))
+            out = cv.warpPerspective(img, Homography_left, (762, 500))
         else:
-            out = cv.warpPerspective(img, Homography_right, (1200, 1000))
+            out = cv.warpPerspective(img, Homography_right, (762, 500))
         # print(out)
         cv.imwrite('/home/mlx/project/src/image/data_set_img/' + name, out)
         # break
@@ -70,3 +66,5 @@ if __name__ == "__main__":
         # k = cv.waitKey(0)
         # if k == 27:
         #     break
+        # elif k=='a':
+        #     continue
